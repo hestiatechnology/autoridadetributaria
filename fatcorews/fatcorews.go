af685d5c076ea932b0a50cb7e44726339a77563d
danielalexis <danielalexis.o.pereira@gmail.com>
2024-08-19 19:38:35 +0100

    feat: Update URLs for fatcorews package

--- fatcorews/fatcorews.go
+++ fatcorews/fatcorews.go
@@ -2,6 +2,10 @@ package fatcorews
 
 import "fmt"
 
+const INTERNAL_URL = "http://fatcorews.gdcontfsimpostosdev.ritta.local:80/fatcorews/ws/"
+const TEST_URL = "https://servicos.portaldasfinancas.gov.pt:723/fatcorews/ws/"
+const PROD_URL = "https://servicos.portaldasfinancas.gov.pt:423/fatcorews/ws/"
+
 func Main() {
 	fmt.Println("Hello, World!")
 }
